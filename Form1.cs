063bb6c8662c0228d0b2b908b01d51c8e12dc6e2
Odailan <44727191+Odailan@users.noreply.github.com>
2018-11-03 14:40:33 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string TrameBrut;
+        bool Valide;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            SPenvoie.Open();
+            SPreception.Open();
+            CbxTrameOkay.Visible = false;
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            
+            TrameBrut = SPreception.ReadExisting();
+            /*TbxTrame.Text = TrameBrut;*/
+            Valide = Traitement(TrameBrut) ;
+            if (Valide == true)
+            {
+                CbxTrameOkay.Visible = true;
+                CbxTrameOkay.Checked = true;
+
+            }
+            else
+            {
+                CbxTrameOkay.Visible = true;
+                CbxTrameOkay.Checked = false;
+
+            }
+        }
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            SPenvoie.Write(tbxTest2.Text);
+            /*tbxTest2.Text = ("non"); 
+            
+            string txt = tbxTest2.Text;
+            tbxTest2.Text = txt.Substring(2, 4);
+            bool Valid = Traitement(tbxTest2.Text);
+            if (Valid == true)
+            {
+                CbxTrameOkay.Checked = true;
+            }
+            else
+            {
+                CbxTrameOkay.Checked = false;
+            }*/
+                
+        }
+
+        public bool Traitement(string tramein)
+        {
+            if (tramein.Substring(0, 3) == "$GP")
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
+        }
+
+        private void BtnMesure_Click(object sender, EventArgs e)
+        {
+            SPreception.Open();
+
+
+
+        }
+    }
+}
